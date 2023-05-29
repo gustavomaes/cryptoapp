7e8aab871e9eb6e72b61a7708e810467a5647929
Gustavo Maes <gustavo.maes@localiza.com>
2023-05-29 16:33:29 -0300

    feat: select tiles

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,22 @@
+import 'package:first_app/models/coin.dart';
 import 'package:first_app/repositories/coin_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final coins = CoinRepository.coins;
+  State<Home> createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  final coins = CoinRepository.coins;
+  List<Coin> selectedCoins = [];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Crypto Coins'),
@@ -15,9 +24,26 @@ class Home extends StatelessWidget {
       body: ListView.separated(
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
-            leading: Image.asset(coins[index].icon),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+            leading: (selectedCoins.contains(coins[index]))
+                ? const CircleAvatar(
+                    child: Icon(Icons.check),
+                  )
+                : SizedBox(
+                    width: 40,
+                    child: Image.asset(coins[index].icon),
+                  ),
             title: Text(coins[index].name),
-            trailing: Text(coins[index].price.toString()),
+            trailing: Text(real.format(coins[index].price)),
+            selected: selectedCoins.contains(coins[index]),
+            selectedTileColor: Colors.deepPurple[50],
+            onLongPress: () => setState(() {
+              selectedCoins.contains(coins[index])
+                  ? selectedCoins.remove(coins[index])
+                  : selectedCoins.add(coins[index]);
+            }),
           );
         },
         padding: const EdgeInsets.all(12),
