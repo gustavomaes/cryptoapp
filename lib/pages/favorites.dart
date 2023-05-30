c1a82d63f40302043f7ddc1993b3f5c604a31e20
Gustavo Maes <gustavo.maes@localiza.com>
2023-05-30 18:37:43 -0300

    feat: list favorites coins

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -1,4 +1,7 @@
+import 'package:first_app/repositories/favorites_repository.dart';
+import 'package:first_app/widgets/coin_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Favorites extends StatelessWidget {
   const Favorites({super.key});
@@ -8,6 +11,30 @@ class Favorites extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Favorites"),
+      ),
+      body: Container(
+        color: Colors.purple.withOpacity(0.05),
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.all(12),
+        child: Consumer<FavoriteRepository>(
+          builder: (context, favorites, child) {
+            return favorites.list.isEmpty
+                ? const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.star),
+                      SizedBox(width: 16),
+                      Text('No favorites yet!'),
+                    ],
+                  )
+                : ListView.builder(
+                    itemCount: favorites.list.length,
+                    itemBuilder: (_, index) {
+                      return CoinCard(coin: favorites.list[index]);
+                    },
+                  );
+          },
+        ),
       ),
     );
   }
---
+++ lib/widgets/coin_card.dart
@@ -0,0 +1,107 @@
+import 'package:first_app/models/coin.dart';
+import 'package:first_app/pages/coin_details.dart';
+import 'package:first_app/repositories/favorites_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+class CoinCard extends StatefulWidget {
+  Coin coin;
+  
+  CoinCard({super.key, required this.coin});
+
+  @override
+  State<CoinCard> createState() => _CoinCardState();
+}
+
+class _CoinCardState extends State<CoinCard> {
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+
+  static Map<String, Color> priceColor = <String, Color> {
+    'up': Colors.teal,
+    'down': Colors.purple,
+  };
+
+  openDetails() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => CoinDetails(coin: widget.coin))
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(top: 12),
+      elevation: 2,
+      child: InkWell(
+        onTap: () => openDetails(),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20, bottom: 20, left: 20),
+          child: Row(
+            children: [
+              Image.asset(widget.coin.icon, height: 40),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(left: 12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.coin.name,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                      Text(
+                        widget.coin.acronym,
+                        style: const TextStyle(
+                          fontSize: 13,
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                decoration: BoxDecoration(
+                  color: priceColor['down']!.withOpacity(0.05),
+                  border: Border.all(
+                    color: priceColor['down']!.withOpacity(0.4),
+                  ),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: Text(
+                  real.format(widget.coin.price),
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: priceColor['down'],
+                    letterSpacing: -1,
+                  ),
+                ),
+              ),
+              PopupMenuButton(
+                icon: const Icon(Icons.more_vert),
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text("Remove"),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Provider.of<FavoriteRepository>(context, listen: false)
+                          .remove(widget.coin);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
